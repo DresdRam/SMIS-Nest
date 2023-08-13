0896405f1efc89dd1fbe80e29c01807c2f368e7e
DresdRam <snipermayv@gmail.com>
2023-08-14 01:46:27 +0300

    Implemented Category Controller and Service

--- src/model/category/category.controller.ts
+++ src/model/category/category.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryDto } from './dto/category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+    constructor(private readonly categoryService: CategoryService) { }
+
+    @Get('get-all')
+    getAllCategories(){
+        return this.categoryService.findAll();
+    }
+
+    @Post('create')
+    createCategory(@Body() body: CategoryDto){
+        return this.categoryService.create(body);
+    }
+
+    @Delete('delete')
+    deleteCategory(@Query('id') id: number){
+        return this.categoryService.delete(id);
+    }
+
+    @Get('get-one')
+    getCategory(@Query('id') id: number){
+        return this.categoryService.findOne(id);
+    }
+
+    @Put('update')
+    updateCategory(@Query('id') id: number, @Body() body: CategoryDto){
+        return this.categoryService.update(id, body);
+    }
+
+}
--- src/model/category/category.service.ts
+++ src/model/category/category.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entity/category.entity';
+import { Repository } from 'typeorm';
+import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+
+    constructor(@InjectRepository(Category) private readonly categoryRepository: Repository<Category>) { }
+
+    async findAll(){
+        return await this.categoryRepository
+        .createQueryBuilder()
+        .select()
+        .getMany();
+    }
+
+    async create(category: CategoryDto){
+        return await this.categoryRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Category)
+        .values([{
+            name: category.name
+        }])
+        .execute();
+    }
+
+    async delete(id: number){
+        return await this.categoryRepository
+        .createQueryBuilder()
+        .delete()
+        .where('id = :id', { id: id })
+        .execute();
+    }
+
+    async findOne(id: number){
+        return await this.categoryRepository
+        .createQueryBuilder()
+        .select()
+        .where('id = :id', { id: id })
+        .getOne();
+    }
+
+    async update(id: number, category: CategoryDto){
+        return await this.categoryRepository
+        .createQueryBuilder()
+        .update(Category)
+        .set({
+            name: category.name
+        })
+        .where('id = :id', { id: id })
+        .execute();
+    }
+
+}
